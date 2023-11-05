e11fb81aa7ad716a451f4b07d6fc10ce396db149
ReptiloidAnunak <amanitocornholio@proton.me>
2023-11-05 03:26:18 -0300

    I created the abstract class Message in bot_management app

--- bot_management/models.py
+++ bot_management/models.py
@@ -2,16 +2,22 @@
 from django.db import models
 
 
-class MessageToClient(models.Model):
+# Messages
+class Message(models.Model):
     class Meta:
-        verbose_name = 'Сообщение клиенту'
-        verbose_name_plural = 'Сообщения клиенту'
-
+        abstract = True  # Абстрактный класс - у него не будет таблицы
     title = models.CharField(max_length=50, unique=True, verbose_name='Название')
     text = models.CharField(max_length=500, verbose_name='Текст')
 
 
-class Buttons(models.Model):
+class MessageToClient(Message):
+    class Meta:
+        verbose_name = 'Сообщение клиенту'
+        verbose_name_plural = 'Сообщения клиенту'
+
+
+# Buttons
+class Button(models.Model):
     class Meta:
         abstract = True  # Абстрактный класс - у него не будет таблицы
 
