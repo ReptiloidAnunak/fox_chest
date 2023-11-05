25a15fa59149c3dbb0508a2934d93ae1c82edfe1
ReptiloidAnunak <amanitocornholio@proton.me>
2023-11-05 03:19:43 -0300

    I created the abstract class Buttons in bot_management app

--- bot_management/models.py
+++ bot_management/models.py
@@ -1,3 +1,4 @@
+
 from django.db import models
 
 
@@ -8,3 +9,12 @@ class MessageToClient(models.Model):
 
     title = models.CharField(max_length=50, unique=True, verbose_name='Название')
     text = models.CharField(max_length=500, verbose_name='Текст')
+
+
+class Buttons(models.Model):
+    class Meta:
+        abstract = True  # Абстрактный класс - у него не будет таблицы
+
+    title = models.CharField(max_length=50, unique=True, verbose_name='Название')
+    display_name = models.CharField(max_length=50, verbose_name='Отображаемое название')
+    description = models.CharField(max_length=200, null=True, verbose_name='Функции')
