772d2499d59477d8697b8c2feba874475b22f6da
ReptiloidAnunak <amanitocornholio@proton.me>
2023-11-14 00:12:57 -0300

    there is a problem with pictures in docker container

--- fox_shop/settings.py
+++ fox_shop/settings.py
@@ -131,10 +131,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/app/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "django_static")
 
-MEDIA_URL = '/app/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
