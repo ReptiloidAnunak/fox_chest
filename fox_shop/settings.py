902d24c1212944b9922249ac78a1001c87ebadee
ReptiloidAnunak <amanitocornholio@proton.me>
2023-11-18 03:53:10 -0300

    Static ?

--- fox_shop/settings.py
+++ fox_shop/settings.py
@@ -132,12 +132,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "django_static")
-# MEDIA_ROOT = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, "django_static")
+MEDIA_ROOT = '/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_ROOT = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
