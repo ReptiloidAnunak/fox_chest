483de1e5499b001bf42b4dd9e831c90c68bb7010
ReptiloidAnunak <amanitocornholio@proton.me>
2023-11-18 04:08:36 -0300

    Static: I changed the static root in settings.py

--- fox_shop/settings.py
+++ fox_shop/settings.py
@@ -132,11 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, "django_static")
-MEDIA_ROOT = '/static/'
+STATIC_ROOT = '/static/'
 
 MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_ROOT = '/media/'
 
 # Default primary key field type
