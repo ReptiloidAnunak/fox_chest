9dc0113fcb86277683f6365547734466f2788dd0
ReptiloidAnunak <amanitocornholio@proton.me>
2023-11-22 00:02:54 -0300

    I made the autocreation of the django-superuser

--- endpoint.sh
+++ endpoint.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/bash
-
-FOX_SUPERUSER_NAME=$FOX_SUPERUSER_NAME
-FOX_SUPERUSER_EMAIL=$FOX_SUPERUSER_EMAIL
-FOX_SUPERUSER_PWD=$FOX_SUPERUSER_PWD
+source .env
 
 python manage.py migrate --check
 status=$?
 if [[ status != 0 ]]; then
   python manage.py migrate
-  python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('${FOX_SUPERUSER_NAME}', '${FOX_SUPERUSER_EMAIL}', '${FOX_SUPERUSER_PWD}')"
+  python manage.py shell -c "import os; from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser(os.environ['FOX_SUPERUSER_NAME'], os.environ['FOX_SUPERUSER_EMAIL'], os.environ['FOX_SUPERUSER_PWD'])"
 
 fi
-python manage.py collectstatic --no-input --clear
+#python manage.py collectstatic --no-input --clear
 exec "$@"
